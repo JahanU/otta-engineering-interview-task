f3de184ac624ba35d82d7695797cb416881ccec6
Jahan <34219176+JahanUoL@users.noreply.github.com>
2022-03-25 19:07:00 +0000

    start on task2 similarites

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ Find the two users with the highest similarity.
 */
 
 const csv = require("csvtojson");
-const csvFilePath = "./data/reactions.csv";
+const reactionsCSV = "./data/reactions.csv";
+const jobsCSV = "./data/jobs.csv";
 
 
 async function getReactionsJson() {
-    return await csv().fromFile(csvFilePath);
+    return await csv().fromFile(reactionsCSV);
+}
+async function getJobsJson() {
+    return await csv().fromFile(jobsCSV);
 }
 
 async function getSimilarityScore(user1, user2, data, max) {
@@ -64,10 +68,9 @@ async function getMaxSimilarity() {
     console.log(max, maxUser1, maxUser2);
     return max;
 }
-
-getMaxSimilarity().then((res) => {
-    console.log(res);
-});
+// getMaxSimilarity().then((res) => {
+//     console.log(res);
+// });
 
 
 
@@ -76,4 +79,50 @@ Task 2 -
 The similarity score between two companies is the number of users who like at least one job at both companies. 
 Using both the reactions.csv and jobs.csv data, 
 find the two companies with the highest similarity score.
+
+eg apple and microsoft
+10 users like jobs at appl
+7 users like jobs at microsoft
+find union of users who like jobs at both companies
+
+Jobs.csv -> job_id -> company_id
+Reactions.csv -> job_id -> user_id & direction
 */
+
+async function getCompanySimilarityScore(company1, company2) {
+
+    const reactions = await getReactionsJson();
+    const jobs = await getJobsJson();
+    
+    const company1Jobs = new Set(jobs
+    .filter((job) => job.company_id == company1)
+    .map((job) => job.job_id));
+    
+    const company2Jobs = new Set(jobs
+    .filter((job) => job.company_id == company2)
+    .map((job) => job.job_id));
+
+    
+    let map = new Map(); // <user, user likes both companies>
+
+    for (let row of reactions) {
+        // user jahan, likes jobs from company 1
+        if (company1Jobs.has(row.job_id) && row.direction) {
+            map.set(row.user_id, false); // set key, value false for now
+        }
+
+        // user jahan, likes jobs from company 2
+        if (company2Jobs.has(row.job_id) && row.direction) {
+            if (map.has(row.user_id)) { // found key, already likes job from company1
+                map.set(row.user_id, true);
+            }
+        }
+    }
+    return { similarityScore: map.size, company1_id: company1, company2_id: company2 };
+}
+
+
+getCompanySimilarityScore(225, 225).then((res) => {
+    console.log(res);
+});
+
